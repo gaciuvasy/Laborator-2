63071283ad851028d3e9860a4aca1f112263dc02
Vasile <gaciuvasy@yahoo.com>
2025-10-27 19:55:20 +0200

    Added publisherAddress

--- Laborator 2/Migrations/20251027173145_AddPublisherAdress.Designer.cs
+++ Laborator 2/Migrations/20251027173145_AddPublisherAdress.Designer.cs
@@ -4,6 +4,7 @@ using Gaciu_Vasile_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Laborator_2.Migrations
 {
     [DbContext(typeof(Gaciu_Vasile_Lab2Context))]
-    partial class Gaciu_Vasile_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20251027173145_AddPublisherAdress")]
+    partial class AddPublisherAdress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Laborator 2/Migrations/20251027173145_AddPublisherAdress.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Laborator_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublisherAdress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublisherAdress",
+                table: "Publisher",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublisherAdress",
+                table: "Publisher");
+        }
+    }
+}
